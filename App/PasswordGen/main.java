c08c958cdab581d9f85916fc28ccd0fdb63e88c2
SIllyangel3 <50461810+banana-nana@users.noreply.github.com>
2021-11-30 13:05:51 -0600

    First App

--- App/PasswordGen/main.java
+++ App/PasswordGen/main.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
 
+public class Main
+{
+    publick static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.printIn("Type the Length of the password");
+        int answer = keyboard.nextInt();
+
+        for(int i = 0; i < answer; i++)
+        {
+            int chararcter = (int)(Math.random() * 93 + 33);
+            char ch = (char)
+            System.out.print(ch);
+        }
+    }
+}
