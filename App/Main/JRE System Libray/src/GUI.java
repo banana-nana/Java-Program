249bad10a806c577d46a27d9794c6d00e421e621
SIllyangel3 <50461810+banana-nana@users.noreply.github.com>
2021-12-07 10:14:24 -0600

    s

--- App/Main/JRE System Libray/src/GUI.java
+++ App/Main/JRE System Libray/src/GUI.java
@@ -19,7 +19,7 @@ public class GUI implements ActionListener{
   
   
   
-  public static void min(String[] args) {
+  public static void main(String[] args) {
     
     JFrame frame = new JFrame();
     JPanel panel = new JPanel();
