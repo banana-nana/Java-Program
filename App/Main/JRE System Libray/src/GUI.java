ad38e328fae9d8d357a9bfebbf95dcb50bb448d3
SIllyangel3 <50461810+banana-nana@users.noreply.github.com>
2021-12-07 08:55:43 -0600

    Create GUI.java

---
+++ App/Main/JRE System Libray/src/GUI.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class GUI implements ActionListener{
+  public static void min(String[] args) {
+    JFrame frame = new JFrame();
+    frame.setSize(100, 100);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setVisable(true);
+    
+    JPanel panel = new JPanel();
+  }
+}
